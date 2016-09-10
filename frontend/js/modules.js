18ad86fc16839b6c1dc678c635b44243fb609743
Adam Uhlir <uhlir.a@gmail.com>
2016-09-10 19:05:59 +0200

    Adding support for configuration into Angular app.

--- frontend/js/modules.js
+++ frontend/js/modules.js
@@ -3,3 +3,12 @@ angular.module('notes.config', []);
 angular.module('notes.service', ['notes.config', 'angularModalService', 'base64']);
 angular.module('notes.utils', []);
 angular.module('notes.ui', []);
+
+// Frontend client global configuration
+var __env = {};
+
+if(window){
+  Object.assign(__env, window.__env);
+}
+
+angular.module('notes.config').constant('__env', __env);
