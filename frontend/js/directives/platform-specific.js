02decf8fbc28c91db455548b026c4edf822645c0
Adam Uhlir <uhlir.a@gmail.com>
2016-09-10 21:28:49 +0200

    Platform specif directives and their uses.

---
+++ frontend/js/directives/platform-specific.js
@@ -0,0 +1,23 @@
+//Display element only on mobile platform
+angular.module('notes.ui').directive("mobileOnly", function (MOBILE_MODE, $animate) {
+    return {
+        restrict: "A",
+        link: function (scope, element, attrs) {
+            if (!MOBILE_MODE) {
+                $animate.addClass(element, 'ng-hide');
+            }
+        }
+    };
+});
+
+//Display element only on web platform
+angular.module('notes.ui').directive("webOnly", function (MOBILE_MODE, $animate) {
+    return {
+        restrict: "A",
+        link: function (scope, element, attrs) {
+            if (MOBILE_MODE) {
+                $animate.addClass(element, 'ng-hide');
+            }
+        }
+    };
+});
